8f1e37846508cae88237738163f6deb520b562ea
Flávio Martil <flavio.martil@italents.com.br>
2024-10-16 10:45:54 -0300

    Removed localStorage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import localFont from "next/font/local";
 import { useTheme, ThemeProvider } from "../context/ThemeContext";
+import Head from "next/head"; // Importação de "Head" adicionada
 import "../styles/globals.css";
 
 const geistSans = localFont({
@@ -21,6 +22,13 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
 
     return (
         <html lang="pt-BR">
+        <Head>
+            <title>WMSolutions - Agência de Desenvolvimento de Software</title>
+            <meta name="description" content="WMSolutions é uma agência de desenvolvimento de software especializada em criar soluções personalizadas para empresas de diversos setores." />
+            <meta name="keywords" content="WMSolutions, desenvolvimento de software, soluções personalizadas, tecnologia, serviços de TI, web development, app development" />
+            <meta name="author" content="WMSolutions Team" />
+            {/* Adicione mais tags meta aqui conforme necessário */}
+        </Head>
         <body className={`${theme === 'dark' ? 'dark-theme' : 'light-theme'} ${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
         </body>
